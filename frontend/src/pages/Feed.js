faef5c2d46eee4ce39f3235da95d85abfc92efa5
thiago costa <thiagocossta_@hotmail.com>
2019-06-13 14:08:46 -0300

    frontend | novas funcionalidades e real time

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Feed.css';
+import api from '../services/api';
+import io from 'socket.io-client';
 
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
@@ -7,56 +9,62 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component{
+    state = {
+        feed: [],
+    };
+    async componentDidMount(){
+        this.registerToSocket();
+        const response = await api.get('posts');
+        this.setState({ feed: response.data });
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+        socket.on('post', newPost => {
+            this.setState({feed: [newPost, ...this.state.feed]});
+        })
+        socket.on('like', likedPost =>{
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post    
+                )
+            })
+        })
+    }
+
+    handleLike = id =>{
+        api.post(`/posts/${id}/like`);
+    }
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Thiago</span>
-                            <span className="place">Olinda</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="http://localhost:3333/files/antifa1.jpg" alt="" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Mais" />
-                            <img src={comment} alt="Mais" />
-                            <img src={send} alt="Mais" />
-                        </div>
-                        <strong>500 curtidas</strong>
-                        <p>
-                            Loucura binchon
-                            <span>#react #top</span>
-                        </p>
-                    </footer>
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais" />
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                        <footer>
+                            <div className="actions">
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="Mais" />
+                                </button>
+                                <img src={comment} alt="Mais" />
+                                <img src={send} alt="Mais" />
+                            </div>
+                            <strong>{post.likes}</strong>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
 
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Thiago</span>
-                            <span className="place">Olinda</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="http://localhost:3333/files/antifa1.jpg" alt="" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Mais" />
-                            <img src={comment} alt="Mais" />
-                            <img src={send} alt="Mais" />
-                        </div>
-                        <strong>500 curtidas</strong>
-                        <p>
-                            Loucura binchon
-                            <span>#react #top</span>
-                        </p>
-                    </footer>
-
-                </article>
+                    </article>
+                ))}
             </section>
         )
     }
