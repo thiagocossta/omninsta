84f7240c3e6ec0e29e8e5d4b98969b85ba341755
thiago costa <thiagocossta_@hotmail.com>
2019-06-13 01:49:04 -0300

    Configurando page style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component{
     render(){
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Thiago</span>
+                            <span className="place">Olinda</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src="http://localhost:3333/files/antifa1.jpg" alt="" />
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="Mais" />
+                            <img src={comment} alt="Mais" />
+                            <img src={send} alt="Mais" />
+                        </div>
+                        <strong>500 curtidas</strong>
+                        <p>
+                            Loucura binchon
+                            <span>#react #top</span>
+                        </p>
+                    </footer>
+
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Thiago</span>
+                            <span className="place">Olinda</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src="http://localhost:3333/files/antifa1.jpg" alt="" />
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="Mais" />
+                            <img src={comment} alt="Mais" />
+                            <img src={send} alt="Mais" />
+                        </div>
+                        <strong>500 curtidas</strong>
+                        <p>
+                            Loucura binchon
+                            <span>#react #top</span>
+                        </p>
+                    </footer>
+
+                </article>
+            </section>
         )
     }
 }
-
 export default Feed;
